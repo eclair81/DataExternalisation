e012a8d97c631da66f886054e2b35b0b129cb952
eclair81 <bastienpic.bp@gmail.com>
2022-04-27 19:32:40 +0200

    {Add} LevelGenerator
    for now, print the color of all the pixels (except the transparent ones) of a Texture2D

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private Elem[] elems;
+
+    void Start()
+    {
+        elems = JsonReader.Instance.stages.colorToElem;
+        //Test, print all the pixel of stage1
+        //GenerateLevel(JsonReader.Instance.maps[0]);
+    }
+
+    public void GenerateLevel(Texture2D map)
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y, map);
+            }
+        }
+    }
+
+    public void GenerateTile(int x, int y, Texture2D map)
+    {
+        Color pixel = map.GetPixel(x, y);
+
+        if(pixel.a == 0)
+        {   
+            // ignore transparent pixels
+            return;
+        }
+        Debug.Log(pixel);
+    }
+}
