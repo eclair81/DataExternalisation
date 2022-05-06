0e2b1ba8f70037ece42cb63c5a88530930837b01
eclair81 <bastienpic.bp@gmail.com>
2022-05-06 20:02:38 +0200

    {Add} GameManager
    static script to store some useful informations

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager : object
+{
+    public static int livesLeft;
+    public static int coinNumber = 0;
+    public static int coinForExtraLife;
+    private static Vector2 playerSavedPos;
+    
+
+    public static void GainCoin()
+    {
+        coinNumber++;
+        if(coinNumber == coinForExtraLife)
+        {
+            Debug.Log("Yay, an extra life!");
+            coinNumber = 0;
+            livesLeft++;
+        }
+        //Debug.Log("Picked up a coin! Current number of coins: " + coinNumber);
+    }
+
+    public static void SavePos(Vector2 pos)
+    {
+        //Debug.Log("Checkpoint!");
+        playerSavedPos = pos;
+    }
+
+    public static Vector2 GetPos()
+    {
+        return playerSavedPos;
+    }
+
+    public static void NextLevel()
+    {
+        Debug.Log("Loading next level...");
+        //To Do
+    }
+}
